845fb529aeaed14fb5b6f209109446636b16822d
gabdrahmanova <78681842+gabdrahmanova@users.noreply.github.com>
2021-02-07 12:45:21 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,14 @@
+a = 10
+classmates = [] 
+# a = a-5.0
+# print(a)
+# print('Hello World')
+# classmates.append('Kate')
+# classmates.append('tahy strico')
+print(classmates)
+while a > 0:
+    print(a)    
+    classmates.append(a)
+    a = a-1
+    
+
